26e72f3d27f7965d48a1809f56966f85b75af4e3
Gediminas_Misevicius <misev.gediminas@gmail.com>
2018-07-03 15:27:53 +0300

    Working array

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ function createButtons() {
     val = ['AC', '±', '%', '÷', '7', '8', '9', '×', '4', '5', '6', '-', '1', '2', '3', '+', '0', 'DEL', ',', '='];
     for (var i=0; i<val.length; i++) {
         HTML += '<div class="button-container">\
-        <button class="button">' + val[i] + '</button>\
+        <input class="button" name="' + val[i] + '" value="' + val[i] + '" />\
         </div>'
         $('.calc-button-container').html(HTML);
     }
@@ -18,11 +18,79 @@ function createButtons() {
 
 function paintRightCol() {
     for(var i=0; i<5; i++) {
-       $('.calc-button-container div:eq(' + (3 + i * 4) + ') button').css('background-color', '#F57E72').css('color', '#fff');
+        $('.calc-button-container div:eq(' + (3 + i * 4) + ') input').css('background-color', '#F57E72').css('color', '#fff');
     }
 }
 
+
+var number_string = '',
+    action_array = [];
+
 $('.button-container').on('click', '.button', function() {
+    
+
+    value = $(this).val();
+
+    if (!isNaN(parseInt(value))) {
+        number_string += value;
+
+        num_len = number_string.length;
+        number = 0;
+
+        for ( var i=0; i<num_len; i++) {
+            number += number_string[i] * 10 ** (num_len - i -1);
+        }
+
+
+
+    } else {
+        
+        action_array.push(number);
+        action_array.push(value);
+
+        switch (value){
+            case 'AC':
+
+            break;
+            case '±':
+
+            break;
+            case '%':
+
+            break;
+            case '÷':
+
+            break;
+            case '×':
+
+            break;
+            case '-':
+
+            break;
+            case '+':
+
+            break;
+            case 'DEL':
+
+            break;
+            case ',':
+
+            break;
+            default:
+            result
+            return result
+        }
+
+    }
+    
+
+    console.log(number_string, 'afaf', number, 'arar', action_array);
+
+
+   
+        
+
+
 
 
 });
